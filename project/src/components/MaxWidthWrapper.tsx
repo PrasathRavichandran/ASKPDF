0214d3d932ea88438ba5fa2f91c8b5b2824747f9
PrasathRavichandran <prasath.webby@gmail.com>
2024-11-09 14:37:59 +0530

    project init done.

---
+++ project/src/components/MaxWidthWrapper.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from "react";
+
+type Props = {
+  className?: string;
+  children: ReactNode;
+};
+
+export default function MaxWidthWrapper({ className, children }: Props) {
+  return (
+    <div className={"mx-auto w-full max-w-screen-xl px-2.5 md:px-20"}>
+      {children}
+    </div>
+  );
+}
