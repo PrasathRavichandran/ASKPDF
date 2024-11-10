8dcabef81d4fdf7ef531fac1cc149c9630fa9a87
PrasathRavichandran <prasath.webby@gmail.com>
2024-11-10 21:45:18 +0530

    - landing page UI is completed. - Added Grainy effect for the body.

--- project/src/app/page.tsx
+++ project/src/app/page.tsx
@@ -1,5 +1,7 @@
 import InfoBar from "@/components/InfoBar";
 import InfoDescription from "@/components/InfoDescription";
+import InfoImage from "@/components/InfoImage";
+import InfoSteps from "@/components/InfoSteps";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 
 export default function Home() {
@@ -17,6 +19,13 @@ export default function Home() {
         heading="Chat with your documents in a seconds."
         description="Unlock your potential with AskPDF, allowing you to engage in meaningful conversations with any PDF document. Simply upload your file and begin your journey of discovery by asking questions right away!"
       />
+      <div className="relative isolate">
+        <InfoImage src={"/dashboard-preview.jpg"} alt="Product preview" />
+      </div>
+      <InfoSteps />
+      <div className="relative isolate">
+        <InfoImage src={"/file-upload-preview.jpg"} alt="File upload preview" />
+      </div>
     </MaxWidthWrapper>
   );
 }
